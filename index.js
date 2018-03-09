2c24aee23dbe729230dde374cb37641c1af1ea68
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-09 19:27:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const RIGHT_ARROW = 39 // use e.which!
 const ROCKS = []
 const START = document.getElementById('start')
 var killAnim = false
+var myReq
 var gameInterval = null
 /**
  * Be aware of what's above this line,
